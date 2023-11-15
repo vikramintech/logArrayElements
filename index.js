a540ad1e468390916d349af4f8403c6505af781d
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2023-11-15 15:39:57 +0000

    added stylong and index.js file updated

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.clear();
+
+// sample array
+const mixData = [
+  1,
+  56,
+  99,
+  "grapes",
+  "audi",
+  45,
+  true,
+  "proooduct",
+  "javascript",
+  "great",
+  "react",
+  "boolean",
+  false,
+  33,
+  100,
+];
+// give array elements
+function getArrayElements(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+// invoking getArrayElements()
+
+getArrayElements(mixData);
